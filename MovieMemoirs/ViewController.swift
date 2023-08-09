add1076cbdddf0140897f50ee6361254cb41dc0c
Maks <maks.vogtman@gmail.com>
2023-08-09 12:37:53 +0200

    Initial Commit

---
+++ MovieMemoirs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMemoirs
+//
+//  Created by Maks Vogtman on 09/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
